2831916d204744e3d0f0977bf89d81c2fa592152
Ben Nappier <bennappier@gmail.com>
2018-08-08 22:48:50 -0700

    refactor world into multiple subworlds; there be bugs

--- game.go
+++ game.go
@@ -10,10 +10,14 @@ import (
 	"os/exec"
 )
 
-type Board [BOARD_SIZE][BOARD_SIZE]int
-type GameBoard struct {
-	board Board
+type World struct {
+	subWorlds [WORLD_SIZE][WORLD_SIZE]SubWorld
 	coinCount int
+	mux sync.Mutex // remove after coin count refactor
+}
+type Grid [GRID_SIZE][GRID_SIZE]int
+type SubWorld struct {
+	grid Grid
 	mux sync.Mutex
 }
 type Coord struct {
@@ -30,142 +34,172 @@ const PLAYER = 1
 const SNAKE = 2
 const COIN = 3
 const ROCK = 4
-const BOARD_SIZE = 36
+const GRID_SIZE = 8
+const WORLD_SIZE = 2
 
-func placePlayer(gameBoard *GameBoard) {
-	placeElementRandomLocationWithLock(gameBoard, PLAYER)
+// creates a player
+// returns a pair of Coord of World, SubWorld
+func initializePlayer(world *World) (Coord, Coord) {
+	x, y := randomPair(WORLD_SIZE)
+	subWorld := world.subWorlds[x][y]
+
+	return Coord{x: x, y: y}, placeElementRandomLocationWithLock(&subWorld, PLAYER)
 }
 
-func placeSnake(gameBoard *GameBoard) {
-	placeElementRandomLocationWithLock(gameBoard, SNAKE)
+// creates a snake
+// returns a pair of Coord of World, SubWorld
+func placeSnake(world *World) (Coord, Coord) {
+	x, y := randomPair(WORLD_SIZE)
+	subWorld := world.subWorlds[x][y]
+
+	return Coord{x: x, y: y}, placeElementRandomLocationWithLock(&subWorld, SNAKE)
 }
 
-func placeCoin(gameBoard *GameBoard) {
-	placeElementRandomLocationWithLock(gameBoard, COIN)
+// creates a Coin
+// returns a pair of Coord of SubWorld
+func placeCoin(subWorld *SubWorld) Coord {
+	return placeElementRandomLocationWithLock(subWorld, COIN)
 }
 
-func placeRock(gameBoard *GameBoard) {
-	placeElementRandomLocationWithLock(gameBoard, ROCK)
+// creates a Rock
+// returns a pair of Coord of SubWorld
+func placeRock(subWorld *SubWorld) Coord {
+	return placeElementRandomLocationWithLock(subWorld, ROCK)
 }
 
-func placeElementRandomLocationWithLock(gameBoard *GameBoard, element int) {
-	gameBoard.mux.Lock()
+// creates a Rock
+// returns a pair of Coord of SubWorld
+func placeElementRandomLocationWithLock(subWorld *SubWorld, element int) Coord {
+	subWorld.mux.Lock()
 
-	placeElementRandomLocation(gameBoard, element)
+	coord := placeElementRandomLocation(subWorld, element)
 
-	gameBoard.mux.Unlock()
+	subWorld.mux.Unlock()
+
+	return coord
 }
 
-func placeElementRandomLocation(gameBoard *GameBoard, element int) {
-	x := rand.Intn(BOARD_SIZE)
-	y := rand.Intn(BOARD_SIZE)
+func placeElementRandomLocation(subWorld *SubWorld, element int) Coord {
+	x, y := randomPair(GRID_SIZE)
+	coord := Coord{x: x, y: y}
 
-	if gameBoard.board[x][y] == EMPTY {
-		gameBoard.board[x][y] = element
+	if subWorld.grid[x][y] == EMPTY {
+		subWorld.grid[x][y] = element
 	} else {
-		placeElementRandomLocation(gameBoard, element)
+		coord = placeElementRandomLocation(subWorld, element)
+	}
+
+	return coord
+}
+
+func moveCharacter(world *World, subWorldCoord Coord, coord Coord, vector Vector, element int) (Coord, Coord) {
+	subWorld := &world.subWorlds[subWorldCoord.x][subWorldCoord.y]
+
+	subWorld.mux.Lock()
+
+	subWorldCoord, nextCoord := subWorldMove(subWorldCoord, coord, vector)
+
+	checkKillSnake(subWorld, nextCoord)
+	checkPickUpCoin(world, subWorld, nextCoord, element)
+
+	if !checkRock(subWorld, nextCoord) {
+		subWorld.grid[coord.x][coord.y] = EMPTY
+		subWorld.grid[nextCoord.x][nextCoord.y] = element
+	}
+
+	subWorld.mux.Unlock()
+
+	return subWorldCoord, nextCoord
+}
+
+func wrap(base int, add int, max int) int {
+	sum := base + add
+
+	if sum > 0 {
+		return sum % max
+	} else {
+		return max + (sum % max)
 	}
 }
 
-func findPlayer(gameBoard *GameBoard) Coord {
-	return findElement(gameBoard, PLAYER)
+func carry(base int, add int, max int) int {
+	sum := base + add
+
+	if sum > 0 {
+		return sum / max
+	} else {
+		return max + (sum / max)
+	}
 }
 
-func findSnake(gameBoard *GameBoard) Coord {
-	return findElement(gameBoard, SNAKE)
-}
+func subWorldMove(subWorldCoord Coord, gridCoord Coord, vector Vector) (Coord, Coord) {
+	wX := subWorldCoord.x + carry(gridCoord.x, vector.x, GRID_SIZE)
+	wY := subWorldCoord.y + carry(gridCoord.y, vector.y, GRID_SIZE)
 
-func findElement(gameBoard *GameBoard, element int) Coord {
-	x := -1
-	y := -1
+	gX := wrap(gridCoord.x, vector.x, GRID_SIZE)
+	gY := wrap(gridCoord.x, vector.x, GRID_SIZE)
 
-	for i := 0; i < BOARD_SIZE; i++ {
-		for j := 0; j < BOARD_SIZE; j++ {
-			if gameBoard.board[i][j] == element {
-				x = i
-				y = j
-				break
-			}
-		}
+	if isOutOfBound(wX, wY, WORLD_SIZE) {
+		return subWorldCoord, gridCoord
 	}
 
-	return Coord{x: x, y: y}
+	return Coord{x: wX, y: wY}, Coord{x: gX, y: gY}
 }
 
-
-
-func moveCharacter(gameBoard *GameBoard, coord Coord, vector Vector, element int) {
-	gameBoard.mux.Lock()
-
-	nextX := wrapAroundBoard(coord.x + vector.x)
-	nextY := wrapAroundBoard(coord.y + vector.y)
-
-	nextCoord := Coord{x: nextX, y: nextY }
-
-	checkKillSnake(gameBoard, nextCoord)
-	checkPickUpCoin(gameBoard, nextCoord, element)
-
-	if !checkRock(gameBoard, nextCoord) {
-		gameBoard.board[coord.x][coord.y] = 0
-		gameBoard.board[nextX][nextY] = element
-	}
-
-	gameBoard.mux.Unlock()
+func isOutOfBound(x int, y int, bound int) bool {
+	return x < 0 ||  y < 0 || x > bound || y > bound
 }
 
 //No lock
-func checkKillSnake(gameBoard *GameBoard, coord Coord) {
-	if gameBoard.board[coord.x][coord.y] == SNAKE {
+func checkKillSnake(subWorld *SubWorld, coord Coord) {
+	if subWorld.grid[coord.x][coord.y] == SNAKE {
 		fmt.Println("YUM!!!")
 	}
 }
 
 //No lock
-func checkPickUpCoin(gameBoard *GameBoard, coord Coord, element int) {
+func checkPickUpCoin(world *World, subWorld *SubWorld, coord Coord, element int) {
 	if element != PLAYER {
 		return
 	}
 
-	if gameBoard.board[coord.x][coord.y] == COIN {
-		gameBoard.coinCount++
+	if subWorld.grid[coord.x][coord.y] == COIN {
+		world.mux.Lock()
+		world.coinCount++
+		world.mux.Unlock()
 	}
 }
 
 //No lock
-func checkRock(gameBoard *GameBoard, coord Coord) bool {
-	return gameBoard.board[coord.x][coord.y] == ROCK
+func checkRock(subWorld *SubWorld, coord Coord) bool {
+	return subWorld.grid[coord.x][coord.y] == ROCK
 }
 
-func wrapAroundBoard(n int) int {
-	if n == -1 {
-		return BOARD_SIZE - 1
-	}
-	if n == BOARD_SIZE {
-		return 0
-	}
-
-	return n
-}
-
-func printBoard(x Board) {
-	for _, i := range x {
-		for _, j := range i {
-			fmt.Printf("%d ", j)
+func printWorld(world *World) {
+	for wy:=0; wy<WORLD_SIZE; wy++ {
+		for gy:=0; gy<GRID_SIZE; gy++ {
+			for wx:=0; wx<WORLD_SIZE; wx++ {
+				for gx:=0; gx<GRID_SIZE; gx++ {
+					fmt.Printf("%d ", world.subWorlds[wx][wy].grid[gx][gy])
+				}
+				fmt.Printf(" ")
+			}
+			fmt.Println()
 		}
 		fmt.Println()
 	}
+}
+
+
+func printStat(world *World) {
+	fmt.Printf("Coin: %d", world.coinCount)
 	fmt.Println()
 }
 
-func printStat(gameBoard *GameBoard) {
-	fmt.Printf("Coin: %d", gameBoard.coinCount)
-	fmt.Println()
-}
-
-func showGame(gameBoard *GameBoard) {
+func render(world *World) {
 	for {
-		printBoard(gameBoard.board)
-		printStat(gameBoard)
+		printWorld(world)
+		printStat(world)
 		time.Sleep(250 * time.Millisecond)
 		clearScreen()
 	}
@@ -179,7 +213,6 @@ func clearScreen() {
 
 func abs(n int) int {
 	if n < 0 {
-
 		return -n
 	} else {
 		return n
@@ -194,45 +227,106 @@ func convertToOneMove(n int) int {
 	}
 }
 
-func snakeWalk(gameBoard *GameBoard) {
+func findPlayer(subWorld *SubWorld) Coord {
+	return findElement(subWorld, PLAYER)
+}
+
+func findElement(subWorld *SubWorld, element int) Coord {
+	x := -1
+	y := -1
+
+	for i := 0; i < GRID_SIZE; i++ {
+		for j := 0; j < GRID_SIZE; j++ {
+			if subWorld.grid[i][j] == element {
+				x = i
+				y = j
+				break
+			}
+		}
+	}
+
+	return Coord{x: x, y: y}
+}
+
+func snakeWalk(world *World) {
+	subWorldCoord, snakeCoord := placeSnake(world)
+
 	for {
-		snakeLocation := findSnake(gameBoard)
-		if snakeLocation.x == -1 && snakeLocation.y == -1 {
+		if snakeCoord.x == -1 && snakeCoord.y == -1 {
 			return
 		}
 
-		playerLocation := findPlayer(gameBoard)
-
-		diffX := playerLocation.x - snakeLocation.x
-		diffY := playerLocation.y - snakeLocation.y
+		playerLocation := findPlayer(&world.subWorlds[subWorldCoord.x][subWorldCoord.y])
 
 		moveVector := Vector{x: 0, y: 0}
-		if abs(diffX) > abs(diffY) {
-			moveVector.x = convertToOneMove(diffX)
+
+		if isFound(playerLocation) {
+			diffX := playerLocation.x - snakeCoord.x
+			diffY := playerLocation.y - snakeCoord.y
+
+			if abs(diffX) > abs(diffY) {
+				moveVector.x = convertToOneMove(diffX)
+			} else {
+				moveVector.y = convertToOneMove(diffY)
+			}
 		} else {
-			moveVector.y = convertToOneMove(diffY)
+			moveVector = randomVector()
 		}
 
-		moveCharacter(gameBoard, snakeLocation, moveVector, SNAKE)
+		subWorldCoord, snakeCoord = moveCharacter(world, subWorldCoord, snakeCoord, moveVector, SNAKE)
 		time.Sleep(1000 * time.Millisecond)
 	}
 }
 
-func dropGold(gameBoard *GameBoard) {
+func isFound(coord Coord) bool {
+	return coord.x > 0 && coord.y > 0
+}
+
+func randomPair(n int) (int, int) {
+	return rand.Intn(n), rand.Intn(n)
+}
+
+func randomVector() Vector {
+	x := rand.Intn(3) - 1
+	y := rand.Intn(3) - 1
+
+	return Vector{x: x, y: y}
+}
+
+func randomSubWorldCoord() Coord {
+	x, y := randomPair(WORLD_SIZE)
+
+	return Coord{x: x, y: y}
+}
+
+func randomSubWorld(world *World) *SubWorld {
+	coord := randomSubWorldCoord()
+
+	return &world.subWorlds[coord.x][coord.y]
+}
+
+func spawnGoldInWorld(world *World) {
 	for {
-		placeCoin(gameBoard)
+		randomSubWorld := randomSubWorld(world)
+		spawnGoldInSubWorld(randomSubWorld)
 		time.Sleep(4000 * time.Millisecond)
 	}
 }
 
-func startTerminalClient(gameBoard *GameBoard) {
-	go showGame(gameBoard)
+func spawnGoldInSubWorld(subWorld *SubWorld) {
+	placeCoin(subWorld)
+}
 
+func startTerminalClient(world *World) {
 	err := termbox.Init()
 	if err != nil {
 		panic(err)
 	}
 	defer termbox.Close()
+
+	subWorldCoord, playerCoord := initializePlayer(world)
+
+	go render(world)
 
 	for {
 		switch ev := termbox.PollEvent(); ev.Type {
@@ -243,18 +337,16 @@ func startTerminalClient(gameBoard *GameBoard) {
 
 			moveVector := Vector{x: 0, y: 0}
 			if ev.Ch == 119 { // w
-				moveVector = Vector{x: -1, y: 0}
+				moveVector.x = -1
 			} else if ev.Ch == 97 { // a
-				moveVector = Vector{x: 0, y: -1}
+				moveVector.y = -1
 			} else if ev.Ch == 115 { // s
-				moveVector = Vector{x: 1, y: 0}
+				moveVector.x = 1
 			} else if ev.Ch == 100 { // d
-				moveVector = Vector{x: 0, y: 1}
+				moveVector.y = 1
 			}
 
-			playerLocation := findPlayer(gameBoard)
-			moveCharacter(gameBoard, playerLocation, moveVector, PLAYER)
-
+			subWorldCoord, playerCoord = moveCharacter(world, subWorldCoord, playerCoord, moveVector, PLAYER)
 			termbox.Flush()
 		case termbox.EventError:
 			panic(ev.Err)
@@ -262,21 +354,39 @@ func startTerminalClient(gameBoard *GameBoard) {
 	}
 }
 
+func initializeWorld() World {
+	subWorlds := [WORLD_SIZE][WORLD_SIZE]SubWorld{}
+
+	for i:=0; i<WORLD_SIZE; i++ {
+		for j:=0; j<WORLD_SIZE; j++ {
+			subWorlds[i][j] = initializeSubworld()
+		}
+	}
+
+	return World{subWorlds: subWorlds}
+}
+
+func initializeSubworld() SubWorld {
+	subWorld := SubWorld{}
+
+	for i:=0; i < 10; i++ {
+		placeRock(&subWorld)
+	}
+
+	return subWorld
+}
+
+func initializeWorldElements(world *World) {
+	go snakeWalk(world)
+	go spawnGoldInWorld(world)
+}
+
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
 
-	board := Board{}
-	gameBoard := GameBoard{board: board}
+	world := initializeWorld()
 
-	placePlayer(&gameBoard)
-	placeSnake(&gameBoard)
+	initializeWorldElements(&world)
 
-	for i:=0; i < 10; i++ {
-		placeRock(&gameBoard)
-	}
-
-	go snakeWalk(&gameBoard)
-	go dropGold(&gameBoard)
-
-	startTerminalClient(&gameBoard)
+	startTerminalClient(&world)
 }
