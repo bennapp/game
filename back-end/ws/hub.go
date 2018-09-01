9119b2b94f5ca4bce4a6eafa370ddf0315295f03
ben <bennappier@gmail.com>
2018-08-31 20:03:11 -0700

    wip sending playerlocation

--- back-end/ws/hub.go
+++ back-end/ws/hub.go
@@ -1,8 +1,10 @@
 package ws
 
 import (
-	"time"
-)
+	"../wo"
+		"time"
+	"encoding/json"
+	)
 
 // Hub maintains the set of active clients and broadcasts messages to the
 // clients.
@@ -54,8 +56,76 @@ func (h *Hub) run() {
 
 // There is where we should beam the world
 func (h *Hub) beamState() {
+	wo.Init()
+
+	id := 386
+	char := "M"
+
+	player := wo.LoadPlayer(id)
+	player.Avatar = char
+
+	//v := gs.NewVector(-5, -5)
+	//visionDistance := 11
+
+	//for i := 0; i < visionDistance; i++ {
+	//	for j := 0; j < visionDistance; j++ {
+	//		element, valid := wo.NextElement(player.SubWorldCoord, player.GridCoord, v)
+	//		if valid {
+	//			fmt.Printf("%v ", element.String())
+	//		}
+	//		v.X += 1
+	//	}
+	//	v.X = -5
+	//	fmt.Println()
+	//	v.Y += 1
+	//}
+
+	//	{
+	//    globalPlayerLocation: {
+	//      x: '2',
+	//      y: '2',
+	//    },
+	//    coordinates: {
+	//      "0,1": { type: 'coin', id: '33' },
+	//      "3,4": { type: 'rock', id: '-1' },
+	//      "1,1": { type: 'rock', id: '-1' },
+	//    },
+	//    objects: {
+	//      // player: {
+	//      //   "1": {
+	//      //     hp: "10",
+	//      //     alive: "true",
+	//      //     coinCount: "22",
+	//      //   },
+	//      //   "2": {
+	//      //     hp: "7",
+	//      //     alive: "true"
+	//      //   }
+	//      // },
+	//      coin: {
+	//        "33": {
+	//          amount: "11",
+	//        },
+	//      },
+	//      rock: {
+	//        "-1": {}
+	//      }
+	//    },
+	//  };
+
+	gameState := map[string]string{}
+
+	globalPlayerLocation := map[string]string{}
+	globalPlayerLocation["x"] = "10"
+	globalPlayerLocation["y"] = "10"
+
+	globalPlayerLocationString, _ := json.Marshal(globalPlayerLocation)
+	gameState["globalPlayerLocation"] = string(globalPlayerLocationString)
+
+	gameStateAsString, _ := json.Marshal(gameState)
+
 	for {
-		h.broadcast <- []byte("{\"message\": \"woah\"}")
+		h.broadcast <- []byte(gameStateAsString)
 		time.Sleep(1000 * time.Millisecond)
 	}
 }
