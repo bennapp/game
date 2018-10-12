545b2dd3a6b9c217f4d293cdc0a0186642385523
ben <bennappier@gmail.com>
2018-10-12 16:44:41 -0700

    Clean up profile code

--- back-end/websocket_server/websocket_server.go
+++ back-end/websocket_server/websocket_server.go
@@ -2,16 +2,8 @@ package main
 
 import (
 	"../ws"
-	"log"
-	"net/http"
 )
 
-import _ "net/http/pprof"
-
 func main() {
-	go func() {
-		log.Println(http.ListenAndServe("localhost:6060", nil))
-	}()
-
 	ws.RunServer()
 }
